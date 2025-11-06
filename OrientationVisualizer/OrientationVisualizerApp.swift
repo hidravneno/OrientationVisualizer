e8c40c219c1a44e6a891a3c382edcd3dcee4a07a
aramburo reyes francisco eduardo <aramburoeduardo972@gmail.com>
2025-11-05 18:09:57 -0800

    Initial Commit

---
+++ OrientationVisualizer/OrientationVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrientationVisualizerApp.swift
+//  OrientationVisualizer
+//
+//  Created by francisco eduardo aramburo reyes on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct OrientationVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
